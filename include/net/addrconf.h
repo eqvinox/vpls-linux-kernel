97325e05f47fc294f0e65c102a6b3efb35cdef1d
David Lamparter <equinox@diac24.net>
2023-11-07 10:30:28 +0100

    leave TODO breadcrumbs

--- include/net/addrconf.h
+++ include/net/addrconf.h
@@ -49,6 +49,12 @@ struct prefix_info {
 	struct in6_addr		prefix;
 };
 
+/*
+ * TODO items left for the PIO/rule 5.5 code:
+ *
+ * - decide whether this should be writable from userspace via netlink
+ *   (it currently is not)
+ */
 struct ip6_neigh_pio {
 	struct list_head	list;
 	struct rcu_head		rcu;
